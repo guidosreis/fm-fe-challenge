2249cce2d1e62653b0acf917279d2b8fa2ccdc4a
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-21 19:47:47 -0300

    Implemented favorite joke page component

--- src/app/favorite-jokes/favorite-jokes.component.ts
+++ src/app/favorite-jokes/favorite-jokes.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { toArray } from 'lodash';
+
+import { Joke } from '../model/joke';
+import { ApplicationState } from '../store/application-state';
+import { UnlikeJokeAction } from '../store/actions';
+
+function stateToFavoriteJokesSelector(state: ApplicationState): Joke[] {
+  return toArray(state.storeData.jokes);
+}
 
 @Component({
   selector: 'app-favorite-jokes',
   templateUrl: './favorite-jokes.component.html',
   styleUrls: ['./favorite-jokes.component.css']
 })
-export class FavoriteJokesComponent implements OnInit {
+export class FavoriteJokesComponent {
+  favoriteJokes$: Observable<Joke[]>;
 
-  constructor() { }
+  constructor(private store: Store<ApplicationState>) {
+    this.favoriteJokes$ = store.select(stateToFavoriteJokesSelector);
+  }
 
-  ngOnInit() {
+  onJokeUnliked(id: number) {
+    this.store.dispatch(new UnlikeJokeAction(id));
   }
 
 }
