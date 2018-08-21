05d79f110788fc52cc8223ab48904eb4e7e093fc
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-21 15:09:21 -0300

    Created uiStateReducer file

---
+++ src/app/store/reducers/uiStateReducer.ts
@@ -0,0 +1,16 @@
+import { cloneDeep } from 'lodash';
+
+import { TOGGLE_RANDOM_FAVORITE_JOKE } from './../actions';
+import { UiState, INITIAL_UI_STATE } from "../ui-state";
+
+export function uiState(state: UiState = INITIAL_UI_STATE, action: any): UiState {
+  switch(action.type) {
+    case TOGGLE_RANDOM_FAVORITE_JOKE:
+      const newState = cloneDeep(state);
+      newState.randomFavoriteJokes = !newState.randomFavoriteJokes;
+      return newState;
+
+    default:
+      return state;
+  }
+}
