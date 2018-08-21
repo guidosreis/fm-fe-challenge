482e627a0520e38c92bd991dfbec6b09124e557a
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-21 15:11:16 -0300

    Refactored AppModule importing AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// routing
+import { AppRoutingModule } from './app-routing.module';
+
+// components
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
