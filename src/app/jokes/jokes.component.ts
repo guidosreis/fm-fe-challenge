6871c79b43f877fdbd6aeab8bdb36a0e3d3ad1a6
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-21 19:47:15 -0300

    Implemented joke page component

--- src/app/jokes/jokes.component.ts
+++ src/app/jokes/jokes.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { toArray } from 'lodash';
+
+import { Joke } from '../model/joke';
+import { ApplicationState } from '../store/application-state';
+import { LikeJokeAction } from '../store/actions';
+
+function stateToJokesSelector(state: ApplicationState): Joke[] {
+  return toArray(state.storeData.jokes);
+}
 
 @Component({
   selector: 'app-jokes',
   templateUrl: './jokes.component.html',
   styleUrls: ['./jokes.component.css']
 })
-export class JokesComponent implements OnInit {
+export class JokesComponent {
+  jokes$: Observable<Joke[]>;
 
-  constructor() { }
+  constructor(private store: Store<ApplicationState>) {
+    this.jokes$ = store.select(stateToJokesSelector);
+  }
 
-  ngOnInit() {
+  onJokeLiked(joke: Joke) {
+    this.store.dispatch(new LikeJokeAction(joke));
   }
 
 }
