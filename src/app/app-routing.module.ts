9efc6c05c4b1c5f3fb5b808669a8cd7c7be62ec3
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-21 15:28:36 -0300

    Refactored AppRoutingModule adding routes for jokes and favorite-jokes pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { JokesComponent } from './jokes/jokes.component';
+import { FavoriteJokesComponent } from './favorite-jokes/favorite-jokes.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'jokes' },
+  { path: 'jokes', component: JokesComponent },
+  { path: 'favorite-jokes', component: FavoriteJokesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
