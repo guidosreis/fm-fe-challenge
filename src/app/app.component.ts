77811e56c4107d365328d6551e4efbdd8756a1f2
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-22 17:00:18 -0300

    Refactored app component class dispatchig the loadJokesAction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { ApplicationState } from './store/application-state';
+import { LoadJokesAction } from './store/actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private store: Store<ApplicationState>) { }
+
+  ngOnInit() {
+    this.store.dispatch(new LoadJokesAction());
+  }
 }
