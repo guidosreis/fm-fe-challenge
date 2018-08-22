4eed15d1a2d81d96a91d5919d0a586400486c764
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-22 19:21:52 -0300

    Refactored app component class adding the logic to dispatch the action to favorite random jokes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import { ApplicationState } from './store/application-state';
-import { LoadJokesAction } from './store/actions';
+import { LoadJokesAction, ToggleRandomFavoriteJokeAction, LoadJokeAction } from './store/actions';
+import { Joke } from './model/joke';
+import { mapStateToFavoriteJokes } from './favorite-jokes/mapStateToFavoriteJokes';
+
+function mapStateToRandomFavoriteJoke(state: ApplicationState): boolean {
+  return state.uiState.randomFavoriteJokes;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,9 +17,43 @@ import { LoadJokesAction } from './store/actions';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<ApplicationState>) { }
+  randomFavoriteJokes$: Observable<boolean>;
+  favoriteJokes$: Observable<Joke[]>;
+  favoriteJokes: Joke[];
+  timer: any;
+
+  constructor(private store: Store<ApplicationState>) {
+    this.randomFavoriteJokes$ = store.select(mapStateToRandomFavoriteJoke);
+    this.favoriteJokes$ = store.select(mapStateToFavoriteJokes);
+  }
 
   ngOnInit() {
     this.store.dispatch(new LoadJokesAction());
+
+    this.favoriteJokes$
+      .subscribe(favoriteJokes => this.favoriteJokes = favoriteJokes);
+
+    this.randomFavoriteJokes$
+      .subscribe(randomFavoriteJokes => {
+        if (!randomFavoriteJokes) {
+          return clearTimeout(this.timer);
+        };
+
+        this.updateTimer();
+      });
+  }
+
+  updateTimer() {
+    this.timer = setTimeout(() => {
+      if (this.favoriteJokes.length < 10) {
+        this.store.dispatch(new LoadJokeAction());
+      }
+
+      this.updateTimer();
+    }, 1000);
+  }
+
+  onRandomFavoriteJokesToggled() {
+    this.store.dispatch(new ToggleRandomFavoriteJokeAction())
   }
 }
