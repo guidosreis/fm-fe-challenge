ba62575faa1cd346ee83178a9d043f7c6c7694fd
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-22 16:59:33 -0300

    Refactored app module importing the modules and services necessary to implement the store manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 // routing
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +15,14 @@ import { FavoriteJokesComponent } from './favorite-jokes/favorite-jokes.componen
 import { JokeListComponent } from './joke-list/joke-list.component';
 import { HeaderComponent } from './header/header.component';
 
+// services
+import { JokesService } from './services/jokes.service';
+
+// store
+import { reducers } from './store/reducers/index';
+import { INITIAL_APPLICATION_STATE } from './store/application-state';
+import { LoadJokesEffectService } from './store/effects/load-jokes-effect.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,9 +33,17 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, { initialState: INITIAL_APPLICATION_STATE }),
+    EffectsModule.forRoot([
+      LoadJokesEffectService
+    ]),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    JokesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
