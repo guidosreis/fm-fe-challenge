fbe017cc9e110cd8be5734ed3bdcd1303b64bdc7
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-22 19:15:40 -0300

    Refactored load jokes effect service implement an effect for load joke action

--- src/app/store/effects/load-jokes-effect.service.ts
+++ src/app/store/effects/load-jokes-effect.service.ts
@@ -1,4 +1,3 @@
-import { LoadJokesSuccessAction } from './../actions';
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
@@ -6,7 +5,10 @@ import { Observable } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 
 import { JokesService } from '../../services/jokes.service';
-import { LoadJokesAction, LOAD_JOKES } from '../actions';
+import {
+  LoadJokesAction, LOAD_JOKES, LoadJokesSuccessAction,
+  LoadJokeAction, LOAD_JOKE, LikeJokeAction
+} from '../actions';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +22,12 @@ export class LoadJokesEffectService {
     .pipe(
       switchMap(action => this.jokesService.getJokes()),
       map(data => new LoadJokesSuccessAction(data['jokes']))
+    );
+
+  @Effect() joke$: Observable<Action> = this.actions$
+    .ofType<LoadJokeAction>(LOAD_JOKE)
+    .pipe(
+      switchMap(action => this.jokesService.getRandom()),
+      map(data => new LikeJokeAction(data['joke']))
     );
 }
