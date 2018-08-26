cd587ab33abf2bb2bed8cf01b6a5b0cf0afcaa78
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-25 23:02:46 -0300

    Refactored jokes service class implementing like and unlike methods

--- src/app/services/jokes.service.ts
+++ src/app/services/jokes.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { remove } from 'lodash';
 
 import { environment } from '../../environments/environment';
 import { of } from 'rxjs';
+import { Joke } from '../model/joke';
 
 const API = `${environment.API}jokes/`;
 const FAVORITE_JOKES = 'FAVORITE_JOKES';
@@ -23,5 +25,26 @@ export class JokesService {
 
   getFavoriteJokes() {
     return of(JSON.parse(localStorage.getItem(FAVORITE_JOKES)) || {});
+  }
+
+  likeJoke(joke: Joke) {
+    const data = JSON.parse(localStorage.getItem(FAVORITE_JOKES)) || {};
+    let { jokes } = data;
+
+    if (!jokes) { jokes = []; }
+
+    jokes.push(joke);
+    localStorage.setItem(FAVORITE_JOKES, JSON.stringify({jokes}));
+    return of(data);
+  }
+
+  unlikeJoke(id: number) {
+    const data = JSON.parse(localStorage.getItem(FAVORITE_JOKES));
+    let { jokes } = data;
+
+    remove(jokes, j => j.id === id);
+
+    localStorage.setItem(FAVORITE_JOKES, JSON.stringify({jokes}));
+    return of(data);
   }
 }
