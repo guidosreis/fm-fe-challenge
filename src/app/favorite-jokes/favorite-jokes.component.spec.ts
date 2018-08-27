52c1b95f9aee7a36f800925f15b82926543b48f5
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-27 12:19:40 -0300

    Created tests for favorite jokes page component

--- src/app/favorite-jokes/favorite-jokes.component.spec.ts
+++ src/app/favorite-jokes/favorite-jokes.component.spec.ts
@@ -1,19 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { StoreModule, Store } from '@ngrx/store';
+import { random } from 'lodash';
 
 import { FavoriteJokesComponent } from './favorite-jokes.component';
+import jokes from '../model/jokes.mock';
+import * as fromApplicationState from '../store/application-state';
+import * as fromReducer from '../store/reducers';
+import * as fromActions from '../store/actions';
 
-describe('FavoriteJokesComponent', () => {
+fdescribe('FavoriteJokesComponent', () => {
   let component: FavoriteJokesComponent;
   let fixture: ComponentFixture<FavoriteJokesComponent>;
+  let store: Store<fromApplicationState.ApplicationState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FavoriteJokesComponent ]
+      imports: [
+        StoreModule.forRoot(
+          fromReducer.reducers
+        ),
+      ],
+      declarations: [ FavoriteJokesComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.get(Store);
+
+    spyOn(store, 'dispatch').and.callThrough();
+
     fixture = TestBed.createComponent(FavoriteJokesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +39,26 @@ describe('FavoriteJokesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display a list of jokes after the data is loaded', () => {
+    const action = new fromActions.LoadFavoriteJokesSuccessAction(jokes);
+
+    store.dispatch(action);
+
+    component.favoriteJokes$.subscribe(items => {
+      expect(items.length).toEqual(jokes.length);
+    });
+  });
+
+  describe('onJokeUnliked', () => {
+    it('should dispatch an action', () => {
+      const randomId = jokes[random(jokes.length - 1)].id;
+      const action = new fromActions.UnlikeJokeAction(randomId);
+
+      component.onJokeUnliked(randomId);
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    });
   });
 });
