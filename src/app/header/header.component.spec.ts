d71818467e21b9eda8c7669f1c13d1459d1eb2cc
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-27 14:18:56 -0300

    Created tests for header component

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './header.component';
 
-describe('HeaderComponent', () => {
+fdescribe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let switchDe: DebugElement;
+  let switchEl: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [
+        FormsModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +24,25 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.randomFavoriteJokes = false;
+
     fixture.detectChanges();
+
+    switchDe = fixture.debugElement.query(By.css('.c-switch__input'));
+    switchEl = switchDe.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit randomFavoriteJokesToggled event when switch is toggled', () => {
+    spyOn(component.randomFavoriteJokesToggled, 'emit');
+    
+    switchEl.click();
+    fixture.detectChanges();
+
+    expect(component.randomFavoriteJokesToggled.emit).toHaveBeenCalled();
+    expect(component.randomFavoriteJokesToggled.emit).toHaveBeenCalledWith(component.randomFavoriteJokes);
   });
 });
