6de411f942a40ed3bff69a9e95ea2b58e807aac0
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-27 12:51:11 -0300

    Created favorite joke list mock to be used on the tests

---
+++ src/app/model/favorite-jokes.mock.ts
@@ -0,0 +1,30 @@
+import { Joke } from './joke';
+
+const favoriteJokes: Joke[] = [
+  {
+    id: 198,
+    joke: "Do you know why Baskin Robbins only has 31 flavors? Because Chuck Norris doesn't like Fudge Ripple."
+  },
+  {
+    id: 292,
+    joke: "If you were somehow able to land a punch on Chuck Norris your entire arm would shatter upon impact. This is only in theory, since, come on, who in their right mind would try this?"
+  },
+  {
+    id: 94,
+    joke: "Chuck Norris has the greatest Poker-Face of all time. He won the 1983 World Series of Poker, despite holding only a Joker, a Get out of Jail Free Monopoly card, a 2 of clubs, 7 of spades and a green #4 card from the game UNO."
+  },
+  {
+    id: 59,
+    joke: "Chuck Norris once roundhouse kicked someone so hard that his foot broke the speed of light, went back in time, and killed Amelia Earhart while she was flying over the Pacific Ocean."
+  },
+  {
+    id: 286,
+    joke: "The square root of Chuck Norris is pain. Do not try to square Chuck Norris, the result is death."
+  },
+  {
+    id: 34,
+    joke: "The opening scene of the movie &quot;Saving Private Ryan&quot; is loosely based on games of dodgeball Chuck Norris played in second grade."
+  }
+];
+
+export default favoriteJokes;
