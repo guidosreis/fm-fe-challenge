4fecc03c55d36df4d4f1c1ff15ea5b086aba6a10
Guilherme dos Reis <greis.gnr@gmail.com>
2018-08-27 17:21:23 -0300

    Created tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,71 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule, Store } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import * as fromApplicationState from './store/application-state';
+import * as fromReducer from './store/reducers';
+import * as fromActions from './store/actions';
+import { routes } from './app-routing.module';
+import { JokesComponent } from './jokes/jokes.component';
+import { FavoriteJokesComponent } from './favorite-jokes/favorite-jokes.component';
+
+fdescribe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let store: Store<fromApplicationState.ApplicationState>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(routes),
+        StoreModule.forRoot(
+          fromReducer.reducers
+        ),
+      ],
       declarations: [
+        JokesComponent,
+        FavoriteJokesComponent,
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    store = TestBed.get(Store);
+
+    spyOn(store, 'dispatch').and.callThrough();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fm-fe-challenge!');
+  });
+
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
   }));
+
+  it('should dispatch an action to load jokes when created', () => {
+    const action = new fromActions.LoadJokesAction();
+
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  it('should dispatch an action to load favorite jokes when created', () => {
+    const action = new fromActions.LoadFavoriteJokesAction();
+
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  describe('onRandomFavoriteJokesToggled', () => {
+    it('should dispatch an action', () => {
+      const flag = true;
+      const action = new fromActions.ToggleRandomFavoriteJokeAction(flag);
+
+      component.onRandomFavoriteJokesToggled(flag);
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    });
+  });
 });
